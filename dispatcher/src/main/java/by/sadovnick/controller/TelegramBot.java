4103dd4ab259632cefc34f21551c314b3b4cd8b9
Sadonikov Alexander <alecos100@gmail.com>
2023-07-02 20:51:46 +0300

    3.Подключение логирования и ответ бота на прием сообщений

--- dispatcher/src/main/java/by/sadovnick/controller/TelegramBot.java
+++ dispatcher/src/main/java/by/sadovnick/controller/TelegramBot.java
@@ -1,12 +1,16 @@
 package by.sadovnick.controller;
 
+import lombok.extern.log4j.Log4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 @Component
+@Log4j
 public class TelegramBot extends TelegramLongPollingBot {
     @Value("${bot.name}")
     private String botName;
@@ -27,6 +31,23 @@ public class TelegramBot extends TelegramLongPollingBot {
     @Override
     public void onUpdateReceived(Update update) {
         Message originalMessage = update.getMessage();
-        System.out.println(originalMessage.getText());
+        log.debug(originalMessage.getText());
+
+        //ответ в тг боте
+        SendMessage response = new SendMessage();
+        response.setChatId(originalMessage.getChatId().toString());
+        response.setText("Hello from bot");
+        sendAnswerMessage(response);
+    }
+
+    //ответ
+    public void sendAnswerMessage(SendMessage message) {
+        if (message != null) {
+            try {
+                execute(message);
+            } catch (TelegramApiException e) {
+                log.error(e);
+            }
+        }
     }
 }
