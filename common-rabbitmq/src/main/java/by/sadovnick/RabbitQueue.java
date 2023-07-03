068e44084279c27ec80c71fd3f2e5fccae08ed0e
Sadovnikov Alexander <alecos100@gmail.com>
2023-07-03 07:49:36 +0300

    5. Создание модуля для RabbitMq и подключение к dispatcher

---
+++ common-rabbitmq/src/main/java/by/sadovnick/RabbitQueue.java
@@ -0,0 +1,13 @@
+package by.sadovnick;
+
+/**
+ * Класс описаний для RabbitMq.
+ * "Зашить" названия очередей брокера в виде констант в коде - это не best-practice.
+ * На реальных проектах так лучше не делать.
+ */
+public class RabbitQueue {
+    public static final String DOC_MESSAGE_UPDATE = "doc_message_update";
+    public static final String PHOTO_MESSAGE_UPDATE = "photo_message_update";
+    public static final String TEXT_MESSAGE_UPDATE = "text_message_update";
+    public static final String ANSWER_MESSAGE_UPDATE = "answer_message";
+}
